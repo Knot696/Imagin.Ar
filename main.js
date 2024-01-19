85db1c5bf70d70a905e75c97a019d366aa778920
Knot <aleknot.696@gmail.com>
2024-01-19 12:20:59 -0300

    avances

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const nav= document.querySelector('.nav');
+const desplegable= document.querySelector('nav');
 
 nav.addEventListener('click', ()=>{
   nav.classList.toggle('open');
+  desplegable.classList.toggle('desplegado');
 })
